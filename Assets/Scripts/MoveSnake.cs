a303bf0c800b2ce0ce9c541fa9fecf91188d313a
Tobel <tobiasz.salik@gmail.com>
2021-02-15 17:26:36 +0100

    commit tobiasza 1

--- Assets/Scripts/MoveSnake.cs
+++ Assets/Scripts/MoveSnake.cs
@@ -14,6 +14,7 @@ public class MoveSnake : MonoBehaviour
         segments = new List<GameObject>();
         currentDirection = Quaternion.identity;
         addNewHeadSegment(Vector3.zero);
+        segments[0].transform.localScale = new Vector3(1,0,0);
     }
 
     // Update is called once per frame
@@ -23,8 +24,17 @@ public class MoveSnake : MonoBehaviour
             currentDirection *= Quaternion.Euler(0, -90, 0);
             addNewHeadSegment(getHeadPosition());
         }
+        if(Input.GetKeyDown(KeyCode.RightArrow)){
+            currentDirection *= Quaternion.Euler(0, 90, 0);
+            addNewHeadSegment(getHeadPosition());
+        }
         segments[segments.Count-1].transform.localScale += new Vector3(speed*Time.deltaTime, 0, 0);
-        
+        segments[0].transform.localScale -= new Vector3(speed*Time.deltaTime, 0, 0);
+        segments[0].transform.position += segments[0].transform.rotation * new Vector3(2*speed*Time.deltaTime, 0, 0);
+        if(segments[0].transform.localScale.x<0){
+            Destroy(segments[0]);
+            segments.Remove(segments[0]);
+        }        
     }
 
     void addNewHeadSegment(Vector3 position)
@@ -36,6 +46,6 @@ public class MoveSnake : MonoBehaviour
 
     Vector3 getHeadPosition()
     {
-        return segments[segments.Count-1].transform.position + new Vector3(segments[segments.Count-1].transform.localScale.x, 0, 0);
+        return segments[segments.Count-1].transform.position + segments[segments.Count-1].transform.rotation * new Vector3(2*segments[segments.Count-1].transform.localScale.x, 0, 0);
     }
 }
